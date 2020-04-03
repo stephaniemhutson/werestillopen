9e59562e6152da25831603e4cd322a9022ede738
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2020-04-03 14:53:58 -0700

    Add area search to mapbox headers
    uses mapbox geocoder
    still trying to figure out how to do addresses

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {Link, Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 import AddBusinessForm from './addBusinessForm.js';
 import OpenForBusinessList from './openForBusinessList.js';
 import Map from './map.js';
+import {MAPBOX_TOKEN} from './config.js';
+import mapboxgl from 'mapbox-gl';
 
 class App extends React.Component {
   render() {
@@ -25,21 +27,14 @@ class App extends React.Component {
 export default App;
 
 class Home extends React.Component  {
-
     constructor(props) {
     super(props);
-    this.state = {
-      postalCode: 92104
-    };
-  }
-
-  updatePostalCode(code) {
-    this.setState({postalCode: code})
+    mapboxgl.accessToken = MAPBOX_TOKEN;
   }
 
   render() {
     return <div>
-      <Map postalCode={this.state.postalCode} />
+      <Map />
       <h2>Open For Business:</h2>
       <OpenForBusinessList />
       <h2>Add your business and status</h2>
--- src/map.js
+++ src/map.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {MAPBOX_TOKEN} from './config.js';
 import mapboxgl from 'mapbox-gl';
+// import {MapboxGeocoder} from 'mapbox-gl-geocoder';
+import axios from 'axios';
+
+const MAPBOX_BASE_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+var MapboxGeocoder = require('mapbox-gl-geocoder');
+// https://api.mapbox.com/geocoding/v5/mapbox.places/92104.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw
 
 class Map extends React.Component {
   constructor(props) {
@@ -9,23 +15,31 @@ class Map extends React.Component {
       // location of San Diego
       lng: -117,
       lat: 32.7157,
-      zoom: 10
+      zoom: 10,
     };
+
   }
 
   componentDidMount() {
-    mapboxgl.accessToken = MAPBOX_TOKEN;
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
-
+    var geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      // container: this.geocoderContainer
+    });
+    map.addControl(geocoder)
   }
 
   render() {
-    return <div class="mapContainer" ref={el => this.mapContainer = el} />
+
+    return <div>
+      <div className="mapContainer" ref={el => this.mapContainer = el} />
+    </div>
   }
 }
 
