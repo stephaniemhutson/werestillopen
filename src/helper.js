2cb914014842ad28f70b9a7004173d3c76913689
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2020-04-27 14:45:51 -0700

    Update helper to work from the business component and the API.
    Not necessarily a very clean way of handling this. Should consider updating the data coming from the API
    to be in a more javascripty format as soon as we receive it.

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,13 @@
 export function businessIsOpenAtAll(business) {
+  // works for Business component and businesses coming from the API
   return (
     business.is_open ||
+    business.isOpen ||
     business.online ||
     business.delivery ||
     business.take_out ||
-    business.by_appointment
+    business.takeout ||
+    business.by_appointment ||
+    business.byAppointment
   )
 }
